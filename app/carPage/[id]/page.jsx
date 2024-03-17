1618408420d219e7e24a98c39d00445145e0c93e
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-03-18 07:12:01 +1100

    added port 3000

--- app/carPage/[id]/page.jsx
+++ app/carPage/[id]/page.jsx
@@ -3,7 +3,7 @@ import CarForm  from '@/components/CarForm'
 import React from 'react'
 
 const getCarDataById = async (id) => {
-  const res = await fetch(process.env.VERCEL_URL + `Cars/${id}`, {
+  const res = await fetch(process.env.VERCEL_URL + ':3000/' + `Cars/${id}`, {
     cache: "no-store"
   })
   if(!res.ok) {
