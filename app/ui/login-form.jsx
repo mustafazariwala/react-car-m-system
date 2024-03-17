d168db7d9a1b0a96dfa098def7276277a1291f3a
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-03-17 21:50:58 +1100

    @ file branch fix

--- app/ui/login-form.jsx
+++ app/ui/login-form.jsx
@@ -1,16 +1,12 @@
 'use client';
  
-import { lusitana } from '@/app/ui/fonts';
 import {
   AtSymbolIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-// import { Button } from '@/app/ui/button';
 import { useFormState, useFormStatus } from 'react-dom';
-// import { authenticate } from '@/app/lib/actions';
- 
 export default function LoginForm() {
   // const [errorMessage, dispatch] = useFormState(authenticate, undefined);
  
